ded71e5690cdd085bbba45c6e66b9e27dec019a5
Pat Bautista <patzj006@gmail.com>
2017-07-28 10:40:09 +0800

    Keyascii on all item forms

--- client/routes/Items/components/ItemInForm.js
+++ client/routes/Items/components/ItemInForm.js
@@ -2,13 +2,20 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { itemIn } from '../../../actions/items-action';
 
-let ItemInForm = ({currentItem, itemIn}) => {
+let ItemInForm = ({currentItem, itemIn, handleInt}) => {
     return (
         <form className="form-horizontal" method="post" action="#" onSubmit={e => itemIn(currentItem.code, e)}>
             <div className="row">
                 <label htmlFor="quantity" className="control-label col-md-3">Quantity</label>
                 <div className="col-md-5">
-                    <input type="number" name="quantity" min="0" max="999" className="form-control input-sm" defaultValue="0" />
+                    <input type="number"
+                        name="quantity"
+                        min="0"
+                        max="999"
+                        className="form-control input-sm"
+                        defaultValue="0"
+                        onKeyUp={e => handleInt(e)}
+                    />
                 </div>
                 <div className="col-md-4">
                     <button type="submit" className="btn btn-default btn-block btn-sm btn-success">Add</button>
@@ -26,7 +33,12 @@ const mapStateToProps = (state) => {
 
 const mapDispatchToProps = (dispatch) => {
     return {
-        itemIn: (code, e) => dispatch(itemIn(code, e))
+        itemIn: (code, e) => dispatch(itemIn(code, e)),
+        handleInt: e => {
+            if((e.keyCode < 58 && e.keyCode > 47)) {
+                return e.value;
+            }
+        }
     };
 };
 
--- client/routes/Items/components/ItemOutForm.js
+++ client/routes/Items/components/ItemOutForm.js
@@ -2,13 +2,20 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { itemOut } from '../../../actions/items-action';
 
-let ItemOutForm = ({currentItem, itemOut}) => {
+let ItemOutForm = ({currentItem, itemOut, handleInt}) => {
     return (
         <form className="form-horizontal" method="post" action="#" onSubmit={e => itemOut(currentItem.code, e)}>
             <div className="row">
                 <label htmlFor="quantity" className="control-label col-md-3">Quantity</label>
                 <div className="col-md-5">
-                    <input type="number" name="quantity" min="0" max="999" className="form-control input-sm" defaultValue="0" />
+                    <input type="number"
+                        name="quantity"
+                        min="0"
+                        max="999"
+                        className="form-control input-sm"
+                        defaultValue="0"
+                        onKeyUp={e => handleInt(e)}
+                    />
                 </div>
                 <div className="col-md-4">
                     <button type="submit" className="btn btn-default btn-block btn-sm btn-warning">Deduct</button>
@@ -26,7 +33,12 @@ const mapStateToProps = (state) => {
 
 const mapDispatchToProps = (dispatch) => {
     return {
-        itemOut: (code, e) => dispatch(itemOut(code, e))
+        itemOut: (code, e) => dispatch(itemOut(code, e)),
+        handleInt: e => {
+            if((e.keyCode < 58 && e.keyCode > 47)) {
+                return e.value;
+            }
+        }
     };
 };
 
