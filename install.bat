7a7dda277a67c042f457b7fe37483aff07c9584b
Franklin Ancheta <franklinjr.ancheta@lorma.edu>
2017-08-16 15:30:50 +0800

    Added build scripts in install.bat

--- install.bat
+++ install.bat
@@ -3,6 +3,7 @@ md "C:\\data\\db"
 md "C:\\mongo\\logs"
 call mongod --logpath C:\\mongo\\logs\\server.log --install
 call npm install
+call npm run build
 call npm install -g qckwinsvc
 call npm install -g bower
 call npm install -g bower-installer
